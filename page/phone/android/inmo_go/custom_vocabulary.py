20b79a20f0ff2e0acae90f233eb83aa7a0120bc4
ziguiway <ziguiway@163,com>
2024-11-06 13:07:12 +0800

    添加自定义翻译页面

---
+++ page/phone/android/inmo_go/custom_vocabulary.py
@@ -0,0 +1,17 @@
+import logging
+
+from appium.webdriver.common.appiumby import AppiumBy
+
+from base.base_page import BasePageAndroid
+from common.type import DriverType, WifiStatusType
+from common.utils import DriverUtils, ElementUtils
+
+
+class CustomVocabularyPage(BasePageAndroid):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.__loc_add_button = (AppiumBy.ID,"com.inmo.inmoglasses:id/iv_add")
+
+    def click_add_button(self):
+        self.base_click(self.__loc_add_button)
